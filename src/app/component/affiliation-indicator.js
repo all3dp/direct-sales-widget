49a8b12612ee2e884988ca4b111be6bb285621c6
Jannik Keye <jannik.keye@gmail.com>
2017-10-11 16:42:50 +0200

    feat(affiliation-indicator): create component & story

---
+++ src/app/component/affiliation-indicator.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import propTypes from 'Lib/prop-types'
+
+import logo from 'Image/logo.svg'
+
+const AffiliationIndicator = () => (
+  <div
+    className="affiliation-indicator"
+  >
+    <span className="affiliation-indicator__text">POWERED BY</span>
+    <img className="affiliation-indicator__logo" alt="All3DP Logo"src={logo} />
+  </div>
+)
+
+AffiliationIndicator.propTypes = {
+  ...propTypes.component
+}
+
+export default AffiliationIndicator
