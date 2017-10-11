1be6dfafb3df1f90c3534b2b01f615e7c87c0471
Jannik Keye <jannik.keye@gmail.com>
2017-10-11 16:34:36 +0200

    refactor(components): remove obsolete components & stories

--- stories/component/star-rating.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {storiesOf} from '@storybook/react'
-
-import StarRating from 'Component/star-rating'
-
-storiesOf('Star Rating', module)
-  .add('default', () => (
-    <StarRating stars={3} of={5} />
-  ))
