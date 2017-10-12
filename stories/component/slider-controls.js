bdaccd94dfd8bd59b9b9d8ba02922297d806b9d2
Jannik Keye <jannik.keye@gmail.com>
2017-10-12 14:49:18 +0200

    fix(slider-controls): add events to story

--- stories/component/slider-controls.js
+++ stories/component/slider-controls.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import {storiesOf} from '@storybook/react'
+import {action} from '@storybook/addon-actions'
 
 import SliderControls from 'Component/slider-controls'
 
 storiesOf('Slider Controls', module)
   .add('default', () => (
-    <SliderControls material={'PREMIUM PLASTIC "white"'} />
+    <SliderControls
+      material={'PREMIUM PLASTIC "white"'}
+      onClickSlideRight={action('click')}
+      onClickSlideLeft={action('click')}
+    />
   ))
