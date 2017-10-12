4a5a10a62bbab98f4f2bd9fe1f83df26d5bae2de
Jannik Keye <jannik.keye@gmail.com>
2017-10-12 09:56:59 +0200

    refactor(affiliation-indicator): format jsx

--- src/app/component/affiliation-indicator.js
+++ src/app/component/affiliation-indicator.js
@@ -5,9 +5,7 @@ import propTypes from 'Lib/prop-types'
 import logo from 'Image/logo.svg'
 
 const AffiliationIndicator = () => (
-  <div
-    className="affiliation-indicator"
-  >
+  <div className="affiliation-indicator">
     <span className="affiliation-indicator__text">POWERED BY</span>
     <img className="affiliation-indicator__logo" alt="All3DP Logo"src={logo} />
   </div>
