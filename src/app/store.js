f05ddbb2f12784304ff39b3ca4b116056a0c3b4f
Jannik Keye <jannik.keye@gmail.com>
2017-10-20 14:48:33 +0200

    remove modal

--- src/app/store.js
+++ src/app/store.js
@@ -7,7 +7,6 @@ import {captureException} from 'Service/logging'
 import {track as trackMixpanel} from 'Service/mixpanel'
 import {track as trackGoogleAnalytics} from 'Service/google-analytics'
 
-import {openFatalErrorModal} from 'Action/modal'
 import rootReducer from './reducer'
 
 const fatalErrorHandler = store => next => (action) => {
@@ -16,7 +15,6 @@ const fatalErrorHandler = store => next => (action) => {
   if (promise && promise.catch) {
     return promise.catch((error) => {
       captureException(error) // log in sentry
-      store.dispatch(openFatalErrorModal(error))
       throw error  // Throw error again
     })
   }
