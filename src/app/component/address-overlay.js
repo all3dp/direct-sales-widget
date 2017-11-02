e20daac6896178c7592cb91c201e58696c90d833
Jannik Keye <jannik.keye@gmail.com>
2017-11-02 09:52:04 +0100

    fix(address-overlay): linter

--- src/app/component/address-overlay.js
+++ src/app/component/address-overlay.js
@@ -16,16 +16,16 @@ const AddressOverlay = ({
   onChange,
   address
 }) => (
-    <div className="address-overlay">
-      <Headline label="Shipping address required" />
-      <Paragraph modifiers={['centered']}>We need your address to calculate the shipping prices!</Paragraph>
-      <LocationField
-        value={formatAddress(address)}
-        placeholder="Set your location"
-        googleMapsApiKey={config.googleMapsApiKey}
-        onChange={onChange}
-      />
-    </div>
+  <div className="address-overlay">
+    <Headline label="Shipping address required" />
+    <Paragraph modifiers={['centered']}>We need your address to calculate the shipping prices!</Paragraph>
+    <LocationField
+      value={formatAddress(address)}
+      placeholder="Set your location"
+      googleMapsApiKey={config.googleMapsApiKey}
+      onChange={onChange}
+    />
+  </div>
   )
 
 AddressOverlay.propTypes = {
