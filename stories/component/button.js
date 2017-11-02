eda5c9cfbf07bfac22286bd6e7903d4a8c55ea05
Jannik Keye <jannik.keye@gmail.com>
2017-11-02 14:19:54 +0100

    add story for loading button

--- stories/component/button.js
+++ stories/component/button.js
@@ -3,6 +3,7 @@ import {storiesOf} from '@storybook/react'
 import {action} from '@storybook/addon-actions'
 
 import Button from 'Component/button'
+import LoadingIndicator from 'Component/loading-indicator'
 
 storiesOf('Button', module)
   .add('default', () => (
@@ -10,4 +11,7 @@ storiesOf('Button', module)
   ))
   .add('disabled', () => (
     <Button label="Disabled Button" disabled onClick={action('click')} />
+  ))
+  .add('loading', () => (
+    <Button label={<LoadingIndicator modifiers={['white']} />} onClick={action('click')} />
   ))
